96b9700e721ca1f8600068e7871cb0cc1e8535b1
dongyoung <ehdanddud@naver.com>
2017-04-04 09:35:49 +0900

    all hello.

--- DongYoung/src/Young.java
+++ DongYoung/src/Young.java
@@ -5,6 +5,7 @@ public class Young {
 		
 		System.out.println("hello world");
 		System.out.println("hello his world");
+		System.out.println("hello her world");
 		System.out.println("hello your world");
 		
 		int a;
